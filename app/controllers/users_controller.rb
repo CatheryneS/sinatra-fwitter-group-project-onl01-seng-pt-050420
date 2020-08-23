e2855c4e0a262566331c8573b42f144459ef0e86
Catheryn Small <catheryn.small@gmail.com>
2020-08-22 22:32:02 -0400

    enabled homepage and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 
+    get '/login' do
+        erb :"/users/login"
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:user][:username])
+        
+        if @user && @user.authenticate(params[:user][:password])
+          session[:id] = @user.id
+          redirect "/users/#{@user.id}"
+        else
+          redirect '/login'
+        end
+    end
+
+    get '/signup' do
+        erb :'/users/signup'
+    end
+
 
 end
